d29ea5014449b64b544cfe074007d1cd205d1653
uwimigisha moise <uwimigisham@gmai.com>
2025-05-17 15:51:28 +0200

    greaat job

---
+++ 8kyu/average.js
@@ -0,0 +1,12 @@
+function updateLight(current) {
+    if (current === "green") {
+        return "yellow";
+    }
+    else if (current === "yellow") {
+        return "red";
+    }
+    else {
+        return "green";
+    }
+
+}
